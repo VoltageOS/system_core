42d5c0e55716709fcf9181636be7fc7ed3007e8c
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2016-05-24 15:12:11 -0700

    healthd: Reinitialize mChargerNames for every battery update
    Booting up the device without usb, the kernel sets the usb power supply
    type as UNKNOWN. The type of usb power supply changes at run-time as
    various chargers are plugged in/out. However, healthd initilizes the
    charger list only at bootup. Change it such that it checks for charger
    type changes with every battery or usb uevent.
    
    While at it, the kernel may have a power supply type which is not known
    to healthd. This is perfectly fine. Update healthd to not print a
    warning.
    
    Change-Id: I2ec9f9a420ca61814d43c316b418ce94de3691bc

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -339,7 +339,6 @@ static BatteryMonitor::PowerSupplyType readRawPowerSupplyType(const String8& pat
 
     auto ret = mapSysfsString(buf.c_str(), supplyTypeMap);
     if (!ret) {
-        KLOG_WARNING(LOG_TAG, "Unknown power supply type '%s'\n", buf.c_str());
         *ret = BatteryMonitor::ANDROID_POWER_SUPPLY_TYPE_UNKNOWN;
     }
 
@@ -558,6 +557,32 @@ void BatteryMonitor::updateValues(void) {
     }
 
     double MaxPower = 0;
+    String8 path;
+
+    // Rescan for the available charger types
+    std::unique_ptr<DIR, decltype(&closedir)> dir(opendir(POWER_SUPPLY_SYSFS_PATH), closedir);
+    if (dir == NULL) {
+        KLOG_ERROR(LOG_TAG, "Could not open %s\n", POWER_SUPPLY_SYSFS_PATH);
+    } else {
+        struct dirent* entry;
+
+        while ((entry = readdir(dir.get()))) {
+            const char* name = entry->d_name;
+
+            if (!strcmp(name, ".") || !strcmp(name, "..")) continue;
+
+            const PowerSupplyType ty = readPowerSupplyType(path, name);
+
+            // Handle device if it's a charger, otherwise do nothing.
+            updateChargerPresence(name, ty);
+
+            // If it's a battery, get whatever battery information is available
+            // from it.
+            if (ty == ANDROID_POWER_SUPPLY_TYPE_BATTERY) {
+                initWithBatteryDevice(name);
+            }
+        }
+    }
 
     for (const auto& chargerName : mChargerNames) {
         String8 path;
